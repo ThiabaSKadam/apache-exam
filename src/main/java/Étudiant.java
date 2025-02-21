e7c0693951e55d970a57bc1ab8bf3a9fcf6ea994
Thiaba Sembène Kadam <kthiabasembene@gmail.com>
2025-02-21 11:44:41 +0000

    Étudiant.java

--- src/main/java/Étudiant.java
+++ src/main/java/Étudiant.java
@@ -1,5 +1,5 @@
-                                       
-/*
+   -Matricule :int, -prenom :String,
+    /* -nom:String, -age int, -idFiliere int                                
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
